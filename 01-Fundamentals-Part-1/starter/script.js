ff92b9ab7ddf9dd632718ccddb9142d7507fa6ed
dattappa <dattappa@outlook.com>
2022-02-19 10:42:51 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,59 @@
+let js = "amazing";
+
+//if (js === "amazing") alert("javascript is amazing");
+//console.log(40 + 8 + 10);
+/* basic assignments
+let firstName = 'Hiran';
+let age = 14;
+
+console.log(firstName);
+console.log(age);
+
+let country;
+let continent;
+let population;
+
+country = 'India';
+continent = 'Asia';
+population = 1300000000;
+
+console.log(country + ' ' + continent + ' ' + population);
+end of basic assignments */ 
+
+
+//primitive data types
+//7 types
+// number, strings, boolean, undefined, null, symbol, bigint
+//dynamically typed : do not need to define type during declaration
+/* data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof (javascriptIsFun));
+console.log(typeof (true));
+console.log(typeof (20));
+console.log(typeof ('hiran'));
+
+let year;
+console.log(typeof (year));
+console.log(year);
+year = 1990;
+console.log(typeof (year));
+console.log(year);
+console.log(null);
+*/
+
+/*let, const and var
+//let is for mutation
+//example let x= 10;
+// x=11;
+
+//const cannot be changed ans always should be initialized
+
+var is old way to define variables prior to es6
+*/
+
+/* 
+***************OPERATORS**********************************
+
+**********************************************************
+*/
